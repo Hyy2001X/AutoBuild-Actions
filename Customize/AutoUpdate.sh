69e6671a8fe97a27b4033fbdb5f5786f795ee96c
A5123 <66279009+A5123@users.noreply.github.com>
2020-10-31 22:18:47 +0800

    Update AutoUpdate.sh

--- Customize/AutoUpdate.sh
+++ Customize/AutoUpdate.sh
@@ -4,8 +4,8 @@
 # AutoUpdate
 
 Version=V3.9
-DEFAULT_DEVICE=d-team_newifi-d2
-Github=https://github.com/Hyy2001X/AutoBuild-Actions
+DEFAULT_DEVICE=型号:Newifi 3 版本:D2
+Github=https://github.com/A5123/AutoBuild-Actions
 
 TIME() {
 	echo -ne "\n[$(date "+%H:%M:%S")] "
