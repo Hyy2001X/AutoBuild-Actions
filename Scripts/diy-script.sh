095e39aedfdc826a4ac0979519c9bd6b384a25ad
beiyuqu <326936595@qq.com>
2020-12-05 21:31:12 +0800

    Update diy-script.sh

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -32,6 +32,8 @@ ExtraPackages svn other smartdns https://github.com/project-openwrt/openwrt/trun
 ExtraPackages git other OpenClash https://github.com/vernesong master
 ExtraPackages git other luci-app-serverchan https://github.com/tty228 master
 ExtraPackages svn other luci-app-socat https://github.com/project-openwrt/openwrt/trunk/package/lienol
+ExtraPackages git passwall https://github.com/xiaorouji/openwrt-passwall.git
+
 # [UPX 压缩] ExtraPackages git other openwrt-upx https://github.com/Hyy2001X master
 # [应用过滤] ExtraPackages git OAF openwrt-OpenAppFilter https://github.com/Lienol master
 # [AdGuardHome 核心] ExtraPackages svn other AdGuardHome https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
