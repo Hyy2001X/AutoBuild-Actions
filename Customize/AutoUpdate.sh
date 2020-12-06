b6628515044a726ec8a736d9d6f92d8fb37742ea
xbfree <39242686+xbfree@users.noreply.github.com>
2020-12-06 19:21:24 +0800

    Update AutoUpdate.sh

--- Customize/AutoUpdate.sh
+++ Customize/AutoUpdate.sh
@@ -3,10 +3,9 @@
 # AutoBuild Module by Hyy2001
 # AutoUpdate
 
-Version=V4.0
+Version=V4.2
 DEFAULT_DEVICE=d-team_newifi-d2
 Github=https://github.com/xbfree/AutoBuild-Actions
-
 
 TIME() {
 	echo -ne "\n[$(date "+%H:%M:%S")] "
@@ -20,27 +19,35 @@ CURRENT_VERSION=$(awk 'NR==1' openwrt_info)
 CURRENT_DEVICE=$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')
 clear && echo "Openwrt-AutoUpdate Script ${Version}"
 if [[ -z "$1" ]];then
-	Upgrade_Options="-q" && TIME && echo "执行: 保留配置升级"
+	Upgrade_Options="-q" && TIME && echo "执行: 保留配置升级固件"
 else
 	Upgrade_Options="$1"
-	[[ "${Upgrade_Options}" == "-n" ]] && TIME && echo "执行: 不保留配置升级"
+	[[ "${Upgrade_Options}" == "-n" ]] && TIME && echo "执行: 不保留配置升级固件"
 	if [[ "${Upgrade_Options}" == "-x" ]];then
 		Upgrade_Options="-q"
 		Force_Update="1"
 		TIME && echo "执行: 保留配置强制升级"
+	else
+		Force_Update="0"
 	fi
 fi
 opkg list | awk '{print $1}' > /tmp/Package_list
 grep "curl" /tmp/Package_list > /dev/null 2>&1
 if [[ ! $? -ne 0 ]];then
+	Baidu_Check=$(curl -I -s --connect-timeout 5 www.baidu.com -w %{http_code} | tail -n1)
+	[ ! "$Baidu_Check" == 200 ] && TIME && echo "网络连接失败,请检查网络连接后重试!" && exit
 	Google_Check=$(curl -I -s --connect-timeout 5 www.google.com -w %{http_code} | tail -n1)
 	[ ! "$Google_Check" == 200 ] && TIME && echo "Google 连接失败,可能导致固件下载速度缓慢!"
 fi
 grep "wget" /tmp/Package_list > /dev/null 2>&1
 if [[ $? -ne 0 ]];then
-	TIME && read -p "未安装 wget,是否执行安装?[Y/n]:" Choose
-	if [ "${Choose}" == Y ] || [ "${Choose}" == y ];then
-		TIME && echo -e "开始安装 wget,请耐心等待...\n"
+	if [[ "${Force_Update}" == "1" ]];then
+		Choose="Y"
+	else
+		TIME && read -p "未安装[wget],是否执行安装?[Y/n]:" Choose
+	fi
+	if [[ "${Choose}" == Y ]] || [[ "${Choose}" == y ]];then
+		TIME && echo -e "开始安装[wget],请耐心等待...\n"
 		opkg update > /dev/null 2>&1
 		opkg install wget
 	else
@@ -50,14 +57,15 @@ if [[ $? -ne 0 ]];then
 	fi
 fi
 if [[ -z "${CURRENT_VERSION}" ]];then
-	echo -e "\n警告:当前固件版本获取失败!"
+	TIME && echo "警告: 当前固件版本获取失败!"
 	CURRENT_VERSION="未知"
 fi
 if [[ -z "${CURRENT_DEVICE}" ]];then
-	echo -e "\n警告:当前设备名称获取失败,使用预设名称[$DEFAULT_DEVICE]"
+	[[ "${Upgrade_Options}" == "-x" ]] && exit
+	TIME && echo "警告: 当前设备名称获取失败,使用预设名称[$DEFAULT_DEVICE]"
 	CURRENT_DEVICE="${DEFAULT_DEVICE}"
 fi
-TIME && echo "正在检查更新..."
+TIME && echo "正在检查版本更新..."
 [ ! -f /tmp/Github_Tags ] && touch /tmp/Github_Tags
 wget -q ${Github_Tags} -O - > /tmp/Github_Tags
 GET_FullVersion=$(cat /tmp/Github_Tags | egrep -o "AutoBuild-${CURRENT_DEVICE}-Lede-R[0-9]+.[0-9]+.[0-9]+.[0-9]+" | awk 'END {print}')
@@ -68,12 +76,12 @@ if [[ -z "${GET_FullVersion}" ]] || [[ -z "${GET_Version}" ]];then
 fi
 echo -e "\n固件作者: ${Author%/*}"
 echo "设备名称: ${DEFAULT_DEVICE}"
-echo -e "\n当前固件版本:${CURRENT_VERSION}"
-echo -e "云端固件版本:${GET_Version}\n"
+echo -e "\n当前固件版本: ${CURRENT_VERSION}"
+echo -e "云端固件版本: ${GET_Version}"
 if [[ ! ${Force_Update} == 1 ]];then
-	if [ "${CURRENT_VERSION}" == "${GET_Version}" ];then
+	if [[ "${CURRENT_VERSION}" == "${GET_Version}" ]];then
 		TIME && read -p "已是最新版本,是否强制更新固件?[Y/n]:" Choose
-		if [ "${Choose}" == Y ] || [ "${Choose}" == y ];then
+		if [[ "${Choose}" == Y ]] || [[ "${Choose}" == y ]];then
 			TIME && echo -e "开始强制更新固件...\n"
 		else
 			TIME && echo "用户已取消强制更新,即将退出更新程序..."
@@ -85,7 +93,7 @@ fi
 Firmware_Info="${GET_FullVersion}"
 Firmware="${Firmware_Info}.bin"
 Firmware_Detail="${Firmware_Info}.detail"
-echo "云端固件名称:${Firmware}"
+echo "云端固件名称: ${Firmware}"
 cd /tmp
 TIME && echo "正在下载固件,请耐心等待..."
 wget -q ${Github_Download}/${Firmware} -O ${Firmware}
@@ -112,6 +120,6 @@ if [[ ! "${GET_MD5}" == "${CURRENT_MD5}" ]];then
 	echo -e "\nMD5对比失败,请检查网络后重试!"
 	exit
 fi
-TIME && echo -e "开始升级固件,请耐心等待...\n"
+TIME && echo -e "MD5 对比通过,开始更新固件,请耐心等待路由器至重启...\n"
 sleep 3
 sysupgrade ${Upgrade_Options} ${Firmware}
