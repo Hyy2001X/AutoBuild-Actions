cfa527d29c50044bbc294bc2a778a8e7c05e8786
zengmiaokun <zengmiaokun@live.com>
2021-02-16 10:46:18 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -4,16 +4,20 @@
 # AutoBuild DiyScript
 
 Diy_Core() {
-	Author=Mango
+	Author=Hyy2001
 	Default_Device=d-team_newifi-d2
 
 	INCLUDE_AutoUpdate=true
 	INCLUDE_AutoBuild_Tools=true
-	INCLUDE_SSR_Plus=false
-	INCLUDE_Passwall=false
-	INCLUDE_Keep_Latest_Xray=false
-	INCLUDE_mt7621_OC1000MHz=false
+	INCLUDE_Keep_Latest_Xray=true
+	INCLUDE_mt7621_OC1000MHz=true
 	INCLUDE_Enable_FirewallPort_53=true
+
+	INCLUDE_SSR_Plus=true
+	INCLUDE_Passwall=false
+	INCLUDE_HelloWorld=false
+	INCLUDE_Bypass=false
+	INCLUDE_OpenClash=true
 }
 
 Diy-Part1() {
@@ -28,19 +32,19 @@ Diy-Part1() {
 	Replace_File Customize/banner package/base-files/files/etc
 	
 	Update_Makefile exfat package/kernel/exfat
+	Replace_File Customize/mt76-20210127.mk package/kernel/mt76 Makefile
+	rm -rf package/kernel/mt76/patches
 
 	# ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
 	# ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
 	# ExtraPackages svn network/services ppp https://github.com/openwrt/openwrt/trunk/package/network/services
 	# ExtraPackages svn network/services hostapd https://github.com/openwrt/openwrt/trunk/package/network/services
-	ExtraPackages svn kernel mt76 https://github.com/openwrt/openwrt/trunk/package/kernel
+	# ExtraPackages svn kernel mt76 https://github.com/openwrt/openwrt/trunk/package/kernel
 
-	ExtraPackages git lean helloworld https://github.com/fw876 master
 	ExtraPackages git lean luci-theme-argon https://github.com/jerrykuku 18.06
 	ExtraPackages git other luci-app-argon-config https://github.com/jerrykuku master
 	ExtraPackages git other luci-app-adguardhome https://github.com/Hyy2001X master
 	ExtraPackages svn other luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
-	ExtraPackages git other luci-app-openclash https://github.com/vernesong/OpenClash/trunk
 	ExtraPackages git other luci-app-serverchan https://github.com/tty228 master
 	ExtraPackages svn other luci-app-socat https://github.com/Lienol/openwrt-package/trunk
 	ExtraPackages svn other luci-app-usb3disable https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw
@@ -49,7 +53,6 @@ Diy-Part1() {
 Diy-Part2() {
 	Diy_Part2_Base
 	Replace_File Customize/mwan3.config package/feeds/packages/mwan3/files/etc/config mwan3
-	# ExtraPackages svn feeds/packages mwan3 https://github.com/openwrt/packages/trunk/net
 }
 
 Diy-Part3() {
