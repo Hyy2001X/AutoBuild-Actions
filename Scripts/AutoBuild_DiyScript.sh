5efa8c683eaa712d6ae6a7f856378eadde6d5ec2
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-05-18 19:06:01 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -5,15 +5,15 @@
 
 Diy_Core() {
 	Author=Thanf
-	Default_Device=d-team_newifi-d2
+	Default_Device=x86_64
 	Short_Firmware_Date=true
 	Default_IP_Address=3.3.3.1
 
 	INCLUDE_AutoUpdate=true
 	INCLUDE_AutoBuild_Tools=true
 	INCLUDE_DRM_I915=true
-	INCLUDE_Theme_Argon=false
-	INCLUDE_Obsolete_PKG_Compatible=true
+	INCLUDE_Theme_Argon=true
+	INCLUDE_Obsolete_PKG_Compatible=false
 }
 
 Firmware-Diy() {
@@ -24,7 +24,7 @@ Firmware-Diy() {
 	d-team_newifi-d2)
 		Replace_File CustomFiles/mac80211.sh package/kernel/mac80211/files/lib/wifi
 		Replace_File CustomFiles/system_d-team_newifi-d2 package/base-files/files/etc/config system
-		Replace_File CustomFiles/Patches/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4
+		# Replace_File CustomFiles/Patches/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4
 	;;
 	esac
 }
